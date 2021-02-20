d976dd8825dd68a3de1915dc593e32d8471d8cf1
Kacy Sommers <kacy@dev.adaptiva.com>
2021-02-20 11:23:25 -0800

    removing lifestyle gallery filters

--- src/core/galleries.js
+++ src/core/galleries.js
@@ -7,7 +7,6 @@ export const Galleries = {
 // View all tag should always be last
 export const GalleryFilters = {
   default: (galleryName) => {
-
     if (galleryName && galleryName.toLowerCase().includes('lifestyle')) {
       const nameSplit = galleryName.split(' ');
       return GalleryFilters[Galleries.LIFESTYLE](nameSplit[0], nameSplit[1]);
@@ -17,133 +16,128 @@ export const GalleryFilters = {
       filters: [
         {
           name: 'View All',
-          keyword: ''
+          keyword: '',
         },
         {
           name: 'Top Picks',
-          keyword: 'showcase'
+          keyword: 'showcase',
         },
         {
           name: 'Featured',
-          keyword: 'featured'
-        }
+          keyword: 'featured',
+        },
       ],
-      keywords: [
-        'showcase',
-        'featured'
-      ],
-      isSpecial: false
-    }
+      keywords: ['showcase', 'featured'],
+      isSpecial: false,
+    };
   },
   [Galleries.LIFESTYLE]: (firstName, lastName) => {
     return {
       filters: [
         {
           name: 'View All',
-          keyword: ''
+          keyword: '',
         },
         {
           name: 'Top Picks',
-          keyword: `showcase+${firstName}+${lastName}+portrait`
+          keyword: `showcase+${firstName}+${lastName}+portrait`,
         },
         {
           name: 'Featured',
-          keyword: 'featured'
-        }
+          keyword: 'featured',
+        },
       ],
       keywords: [
         `showcase\\+${firstName}\\+${lastName}\\+portrait`,
-        'featured'
+        'featured',
       ],
-      isSpecial: false
-    }
+      isSpecial: false,
+    };
   },
-  [Galleries.LIMITED_EDITION]: {
-    filters: [
-      {
-        name: 'View All',
-        keyword: ''
-      },
-      {
-        name: 'Most Popular',
-        keyword: 'Popular'
-      },
-      {
-        name: 'Black & White',
-        keyword: 'BW'
-      },
-      {
-        name: '1970s',
-        keyword: '1970s'
-      },
-      {
-        name: '1980s',
-        keyword: '1980s'
-      },
-      {
-        name: '1990s',
-        keyword: '1990s'
-      },
-      {
-        name: '2000s',
-        keyword: '2000s'
-      },
-      {
-        name: '2010s',
-        keyword: '2010s'
-      },
-      {
-        name: 'Sturgis',
-        keyword: 'Sturgis'
-      },
-      {
-        name: 'Riding',
-        keyword: 'Riding'
-      },
-      {
-        name: 'Competition',
-        keyword: 'Competition'
-      },
-      {
-        name: 'Classic Bikes',
-        keyword: 'Antique'
-      },
-      {
-        name: 'Decorative',
-        keyword: 'Decorative'
-      },
-      {
-        name: 'Naked Truth Exhibition',
-        keyword: 'maa15'
-      },
-      {
-        name: 'Heavy Mettle Exhibition',
-        keyword: 'maa20'
-      }
-    ],
-    keywords: [
-      'Popular',
-      'BW',
-      '1970s',
-      '1980s',
-      '1990s',
-      '2000s',
-      '2010s',
-      'Sturgis',
-      'Riding',
-      'Competition',
-      'Antique',
-      'Decorative',
-      'maa15',
-      'maa20'
-    ],
-    isSpecial: true
-  }
+  // [Galleries.LIMITED_EDITION]: {
+  //   filters: [
+  //     {
+  //       name: 'View All',
+  //       keyword: '',
+  //     },
+  //     {
+  //       name: 'Most Popular',
+  //       keyword: 'Popular',
+  //     },
+  //     {
+  //       name: 'Black & White',
+  //       keyword: 'BW',
+  //     },
+  //     {
+  //       name: '1970s',
+  //       keyword: '1970s',
+  //     },
+  //     {
+  //       name: '1980s',
+  //       keyword: '1980s',
+  //     },
+  //     {
+  //       name: '1990s',
+  //       keyword: '1990s',
+  //     },
+  //     {
+  //       name: '2000s',
+  //       keyword: '2000s',
+  //     },
+  //     {
+  //       name: '2010s',
+  //       keyword: '2010s',
+  //     },
+  //     {
+  //       name: 'Sturgis',
+  //       keyword: 'Sturgis',
+  //     },
+  //     {
+  //       name: 'Riding',
+  //       keyword: 'Riding',
+  //     },
+  //     {
+  //       name: 'Competition',
+  //       keyword: 'Competition',
+  //     },
+  //     {
+  //       name: 'Classic Bikes',
+  //       keyword: 'Antique',
+  //     },
+  //     {
+  //       name: 'Decorative',
+  //       keyword: 'Decorative',
+  //     },
+  //     {
+  //       name: 'Naked Truth Exhibition',
+  //       keyword: 'maa15',
+  //     },
+  //     {
+  //       name: 'Heavy Mettle Exhibition',
+  //       keyword: 'maa20',
+  //     },
+  //   ],
+  //   keywords: [
+  //     'Popular',
+  //     'BW',
+  //     '1970s',
+  //     '1980s',
+  //     '1990s',
+  //     '2000s',
+  //     '2010s',
+  //     'Sturgis',
+  //     'Riding',
+  //     'Competition',
+  //     'Antique',
+  //     'Decorative',
+  //     'maa15',
+  //     'maa20',
+  //   ],
+  //   isSpecial: true,
+  // },
 };
 
-export const safeGalleries = [
-  'limited edition'
-];
+export const safeGalleries = ['limited edition'];
 
 export const getLifestyleQuery = (galleryName) => {
   if (!galleryName) {
@@ -157,4 +151,3 @@ export const getLifestyleQuery = (galleryName) => {
 
   return '';
 };
-
